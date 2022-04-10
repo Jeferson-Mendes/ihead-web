b36b7fc27df0d142303c02b06f4c68425908f154
jeferson-mendes <jefersonmendes3c@gmail.com>
2022-04-10 14:11:55 -0300

    fix: improve path

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Article from '../../components/article';
-import Navbar from '../../components/navbar';
+import Navbar from '../../components/Navbar';
 
 import { 
     SearchContainerStyled,
